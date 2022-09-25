a8cd74f3f5aaad20e9f5e1b867a6fafda6170324
Mariem Mkadem <mariem.mkadem@outlook.com>
2022-09-25 21:19:04 +0200

    setup express

--- server/server.js
+++ server/server.js
@@ -1,1 +1,25 @@
-console.log('server setup')
+import express from 'express';
+import cors from 'cors';
+
+const morgan = require('morgan')
+require("dotenv").config();
+
+//create express app
+
+const app = express();
+
+// apply middelwares
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+
+//route 
+app.get('/', (req, res, next) => {
+    res.send('you hit server endpoint')
+})
+
+//port 
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
