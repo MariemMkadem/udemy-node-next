58c8b9b85649c8280eb6cdce6d10631838198b7f
Mariem Mkadem <mariem.mkadem@outlook.com>
2022-09-25 22:19:42 +0200

    add controller

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,9 @@
 import express from 'express';
 
+//controllers
+import { register } from '../controllers/auth';
 const router = express.Router();
 
-router.get('/register', (req,res)=> {
-    res.send('register user')
-
-})
+router.get('/register', register)
 
 module.exports = router;
