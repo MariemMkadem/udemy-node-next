333ea37fb445a4f218b86e0c2b67f96800168319
Mariem Mkadem <mariem.mkadem@outlook.com>
2022-10-11 10:42:18 +0200

    global state with context api and reducer hook

---
+++ client/context/index.js
@@ -0,0 +1,36 @@
+import { useReducer, createContext } from 'react';
+
+//initial state
+const initialState = {
+    user: null,
+};
+
+//createContext
+const Context = createContext()
+
+// root reducer : used to update the state and to acess data from the state
+// action contient Type et payload : payload constient user informations
+const rootReducer = (state, action) => {
+    switch (action.type) {
+        case "LOGIN":
+            return { ...state, user: action.payload }
+        case "LOGOUT":
+            return { ...state, user: null }
+        default:
+            return state;
+    }
+}
+
+//context provider
+const Provider = ({ children }) => {
+    const [state, dispatch] = useReducer(rootReducer, initialState)
+
+    return (
+        <Context.Provider value={{ state, dispatch }}>
+            {children}
+        </Context.Provider>
+    )
+
+}
+
+export { Context, Provider }
